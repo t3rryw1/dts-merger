23b842b15bbb36c8eceebd2d9e64b07460cd3d40
artour <alchemy313@163.c0m>
2018-05-22 12:28:59 +0800

    add merge test

--- src/test/java/com/cozystay/model/TestSyncTask.java
+++ src/test/java/com/cozystay/model/TestSyncTask.java
@@ -55,6 +55,59 @@ public class TestSyncTask {
         Assert.assertTrue(task1.allOperationsCompleted());
     }
 
+    @Test
+    public void testMergeStatus(){
+        SyncTask task1 = new SyncTaskImpl("id-123-abc","test-db","test-table");
+        SyncTask task2 = new SyncTaskImpl("id-123-abc","test-db","test-table");
+        SyncOperation.SyncItem item = new SyncOperation.SyncItem<>("name", "aa", "bb", SyncOperation.SyncItem.ColumnType.CHAR,true);
+        task1.addOperation(new SyncOperationImpl(null,
+                SyncOperation.OperationType.CREATE,
+                new ArrayList<>(Arrays.asList(item)),
+                "source1",
+                new ArrayList<>(Arrays.asList("source1", "source2")),
+                new Date().getTime()));
+        task2.addOperation(new SyncOperationImpl(null,
+                SyncOperation.OperationType.CREATE,
+                new ArrayList<>(Arrays.asList(item)),
+                "source2",
+                new ArrayList<>(Arrays.asList("source1", "source2")),
+                new Date().getTime()));
+
+        task1.mergeStatus(task2);
+
+        Assert.assertTrue(task1.allOperationsCompleted());
+    }
+
+    @Test
+    public void testDeepMerge(){
+        SyncTask task1 = new SyncTaskImpl("id-123-abc","test-db","test-table");
+        SyncTask task2 = new SyncTaskImpl("id-123-abc","test-db","test-table");
+        SyncOperation.SyncItem item1 = new SyncOperation.SyncItem<>("name", "aa", "bb", SyncOperation.SyncItem.ColumnType.CHAR,true);
+        SyncOperation.SyncItem item2 = new SyncOperation.SyncItem<>("name", "cc", "dd", SyncOperation.SyncItem.ColumnType.CHAR,true);
+        SyncOperation.SyncItem item3 = new SyncOperation.SyncItem<>("xxxx", "aa", "bb", SyncOperation.SyncItem.ColumnType.CHAR,false);
+        SyncOperation.SyncItem item4 = new SyncOperation.SyncItem<>("xxxx", "cc", "dd", SyncOperation.SyncItem.ColumnType.CHAR,false);
+        task1.addOperation(new SyncOperationImpl(null,
+                SyncOperation.OperationType.CREATE,
+                new ArrayList<>(Arrays.asList(item1, item3)),
+                "source1",
+                new ArrayList<>(Arrays.asList("source1", "source2")),
+                new Date().getTime()));
+        task2.addOperation(new SyncOperationImpl(null,
+                SyncOperation.OperationType.CREATE,
+                new ArrayList<>(Arrays.asList(item2, item4)),
+                "source1",
+                new ArrayList<>(Arrays.asList("source1", "source2")),
+                new Date().getTime()));
+
+        task1.deepMerge(task2);
+
+        List<SyncOperation.SyncItem> itemList = task1.getOperations().get(0).getSyncItems();
+
+        Assert.assertEquals(itemList.get(0).currentValue, "dd");
+        Assert.assertEquals(itemList.get(1).currentValue, "dd");
+    }
+
+
     @After
     public void tearDown(){
         System.out.println("tear down " + this.getClass().getName());
