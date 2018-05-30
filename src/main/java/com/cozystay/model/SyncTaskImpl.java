2349a026b591bdd20a99164724718ec9c198ac8b
artour <alchemy313@163.c0m>
2018-05-30 17:27:47 +0800

    optimize merge logic

--- src/main/java/com/cozystay/model/SyncTaskImpl.java
+++ src/main/java/com/cozystay/model/SyncTaskImpl.java
@@ -164,30 +164,31 @@ public class SyncTaskImpl implements SyncTask {
             if(sources.containsKey(sourceName)){
                 sources.get(sourceName).add(operation);
             }else{
-                sources.put(sourceName, new ArrayList<>(Arrays.asList(operation)));
+                sources.put(sourceName, new ArrayList<>(Collections.singletonList(operation)));
             }
         }
 
         SyncTask mergedTask = new SyncTaskImpl(uuid, database, tableName, type);
+        List<SyncOperation> diffOps = new ArrayList<>();
 
         for (Map.Entry<String, List<SyncOperation>> source : sources.entrySet()) {
-            SyncOperation op = deepMergeFromSameSource(source.getValue());
-            mergedTask.addOperation(op);
+            SyncOperation operation = deepMergeOps(source.getValue());
+            diffOps.add(operation);
         }
 
-        List<SyncOperation> concatOps = mergedTask.getOperations();
-        SyncOperation splitOp = getOverWroteOpFromDiffSource(concatOps);
-        mergedTask.addOperation(splitOp);
+        for (SyncOperation operation : fixConflictOpsFromDiffSource(diffOps)) {
+            mergedTask.addOperation(operation);
+        }
 
         return mergedTask;
     }
 
-    private SyncOperation deepMergeFromSameSource(List<SyncOperation> sameOps) {
-        sameOps.sort((o1, o2) -> o1.getTime() > (o2.getTime()) ? -1 : 1);
+    private SyncOperation deepMergeOps(List<SyncOperation> Ops) {
+        Ops.sort((o1, o2) -> o1.getTime() > (o2.getTime()) ? -1 : 1);
 
         SyncOperation acc = null;
         //reduce
-        for (SyncOperation operation : sameOps) {
+        for (SyncOperation operation : Ops) {
             if(acc == null){
                 acc = operation;
                 continue;
@@ -197,7 +198,7 @@ public class SyncTaskImpl implements SyncTask {
         return acc;
     }
 
-    private SyncOperation getOverWroteOpFromDiffSource(List<SyncOperation> diffOps) {
+    private List<SyncOperation> fixConflictOpsFromDiffSource(List<SyncOperation> diffOps) {
         diffOps.sort((o1, o2) -> o1.getTime() > (o2.getTime()) ? -1 : 1);
 
         Map<String, Integer> fieldCount = new HashMap<>();
@@ -211,32 +212,27 @@ public class SyncTaskImpl implements SyncTask {
             }
         }
 
-        List<SyncOperation.SyncItem> splitItems =  new ArrayList<>();
         for (Map.Entry<String, Integer> field : fieldCount.entrySet()) {
             if (field.getValue() > 1) {
-                for (SyncOperation operation : diffOps) {
-                    //remove item from operation & add it to list
-                    SyncOperation.SyncItem item = operation.removeItemByFieldName(field.getKey());
-                    if (item != null) {
-                        splitItems.add(item);
+                //if fieldName count more than once，it means operation which contain this fieldName should be merged
+                List<SyncOperation> needMergeOps = new ArrayList<>();
+                Iterator<SyncOperation> iterativelyOps = diffOps.iterator();
+                while (iterativelyOps.hasNext()) {
+                    SyncOperation operation = iterativelyOps.next();
+                    if (operation.getSyncItems().stream().anyMatch(syncItem -> syncItem.fieldName == field.getKey())) {
+                        //remove conflict operation
+                        iterativelyOps.remove();
+
+                        //add to be merged
+                        needMergeOps.add(operation);
                     }
                 }
+                //merge this conflict operations and put it back
+                diffOps.add(deepMergeOps(needMergeOps));
             }
         }
 
-        SyncOperation splitOp = new SyncOperationImpl(
-                null,
-                diffOps.get(0).getOperationType(),
-                splitItems,
-                diffOps.get(0).getSource(),
-                new ArrayList<>(diffOps.get(0).getSyncStatus().keySet()),
-                new Date().getTime());
-
-        //reset source to INIT
-        splitOp.updateStatus(diffOps.get(0).getSource(), SyncOperation.SyncStatus.INIT);
-        splitOp.reduceItems();
-
-        return splitOp;
+        return diffOps;
     }
 
     public void addOperation(SyncOperation operation) {
